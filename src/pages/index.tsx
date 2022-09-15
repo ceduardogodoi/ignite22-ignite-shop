f2cb404704d09af79a083b32a22ccf1a15664e94
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-09-15 10:34:11 -0300

    Data Fetching no Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ import { HomeContainer, Product } from '../styles/pages/home'
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
 
 import 'keen-slider/keen-slider.min.css'
 
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -17,14 +18,7 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+      <pre>{JSON.stringify(props.list)}</pre>
 
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
@@ -36,7 +30,7 @@ export default function Home() {
       </Product>
 
       <Product className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
+        <Image src={camiseta2} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
@@ -45,7 +39,16 @@ export default function Home() {
       </Product>
 
       <Product className="keen-slider__slide">
-        <Image src={camiseta1} width={520} height={480} alt="" />
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
@@ -54,4 +57,12 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+export const getServerSideProps = () => {
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
 }
