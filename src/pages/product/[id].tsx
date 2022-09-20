074c44dd7e478dcf6dc71879d80d8fe3d379ec26
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-09-19 22:08:08 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,98</span>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia dolore iste, adipisci tempore qui, debitis modi totam error neque, similique assumenda. Atque velit nemo facere ratione mollitia rem numquam in?</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
