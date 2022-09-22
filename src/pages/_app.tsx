ef7a1e10cf335a7f5a2c3a3f13ee4188733bbcc8
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-09-22 09:35:12 -0300

    Botão de sacolinha

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,29 @@
 import { AppProps } from 'next/app'
 import Image from 'next/future/image'
+import { Handbag } from 'phosphor-react'
 import logoImg from '../assets/logo.svg'
 import { globalStyles } from '../styles/global'
-import { Container, Header } from '../styles/pages/app'
+import { Button, Container, Header } from '../styles/pages/app'
 
 globalStyles()
+
+const items = []
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
         <Image src={logoImg} alt="" />
+
+        <Button items={items.length > 0}>
+          <Handbag size={24} weight="bold" />
+
+          {items.length > 0 && (
+            <div>
+              <span>{items.length}</span>
+            </div>
+          )}
+        </Button>
       </Header>
 
       <Component {...pageProps} />
